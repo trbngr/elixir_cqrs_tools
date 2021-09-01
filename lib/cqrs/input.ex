ad8a5f4ad54c7201291e22c4879001f0aaa8cbf0
Chris Martin <chris.martin@oforce.com>
2021-08-31 22:46:29 -0700

    update input normalization

--- lib/cqrs/input.ex
+++ lib/cqrs/input.ex
@@ -6,25 +6,32 @@ defmodule Cqrs.Input do
   def normalize_input(input, mod) do
     mod
     |> normalize(input)
+    |> string_keys()
+    |> destruct()
     |> populate_from_sources(mod)
-    |> destruct_values()
   end
 
-  defp destruct_values(list) when is_list(list), do: Enum.map(list, &destruct_values/1)
+  defp destruct(list) when is_list(list), do: Enum.map(list, &destruct/1)
 
-  defp destruct_values(map) when is_map(map) do
+  defp destruct(map) when is_map(map) do
     Enum.into(map, %{}, fn
-      {key, value} when is_struct(key) -> {to_string(key), destruct_values(Map.from_struct(value))}
-      {key, value} -> {to_string(key), value}
+      {key, value} when is_struct(key) -> {key, destruct(Map.from_struct(value))}
+      {key, value} -> {key, value}
     end)
   end
 
-  defp destruct_values(other), do: other
+  defp destruct(other), do: other
 
-  defp normalize(_mod, values) when is_list(values), do: Enum.into(values, %{})
-  defp normalize(_mod, values) when is_struct(values), do: Map.from_struct(values)
-  defp normalize(_mod, values) when is_map(values), do: values
+  defp normalize(_mod, input) when is_list(input), do: Enum.into(input, %{})
+  defp normalize(_mod, input) when is_struct(input), do: Map.from_struct(input)
+  defp normalize(_mod, input) when is_map(input), do: input
   defp normalize(mod, _other), do: raise(InvalidValuesError, module: mod)
+
+  defp string_keys(input) do
+    for {key, value} <- input, into: %{} do
+      {to_string(key), value}
+    end
+  end
 
   defp populate_from_sources(input, mod) do
     Enum.reduce(mod.__schema__(:fields), input, fn field, acc ->
@@ -33,7 +40,7 @@ defmodule Cqrs.Input do
 
       acc
       |> Map.delete(field_source)
-      |> Map.update(field, source_value, fn
+      |> Map.update(to_string(field), source_value, fn
         nil -> source_value
         other -> other
       end)
